4f5f7a9d7624d615a3423bde4bcee41a4c5908ae
dvarx <dvarx@ethz.ch>
2021-12-09 16:21:51 +0100

    added def file

---
+++ tnb_mns_defs.h
@@ -0,0 +1,23 @@
+/*
+ * tnb_mns_defs.h
+ *
+ *  Created on: Dec 9, 2021
+ *      Author: dvarx
+ */
+
+#ifndef TNB_MNS_DEFS_H_
+#define TNB_MNS_DEFS_H_
+
+#define DEBUG_CLOSED_LOOP
+
+//constants related to output inductor current PI controller
+#define RDC 3.3
+#define VIN 40
+#define CTRL_KP 0.0
+#define CTRL_KI 0.0
+
+//buck duty first order time constants (ms)
+#define TAU_BUCK_DUTY 300e-3
+
+
+#endif /* TNB_MNS_DEFS_H_ */
